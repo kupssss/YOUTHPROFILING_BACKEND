279b07f10b5f2b3ef5c9556ea864058115a4f1d7
unknown <diaz.rj.bscs@gmail.com>
2025-09-19 16:09:08 +0800

    update

--- thesis/settings.py
+++ thesis/settings.py
@@ -19,8 +19,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "youthprofiling.thesis.message@gmail.com"
-EMAIL_HOST_PASSWORD = "gpcwzhvhftuephmb"
+EMAIL_HOST_USER = "youthprofiling.capstone.message@gmail.com"
+EMAIL_HOST_PASSWORD = "ryycyhpllookrqyv"
 
 
 
